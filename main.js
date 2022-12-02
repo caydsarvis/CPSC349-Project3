9225ff69abf98bd98f9f0622316dd9f8f71033b4
danielm19 <danielm19@csu.fullerton.edu>
2022-12-01 18:11:34 -0800

    added a basic winner function

--- main.js
+++ main.js
@@ -14,16 +14,28 @@ let ways_to_win = [
 let player1 = 'X';
 let player2 = 'O';
 let turn = 0;
+let total_rows = 8;
 
 for (let i = 0; i < board.length; i++) {
     board[i].addEventListener('click', (e) => {
         if (turn == 0) {
             board[i].textContent = player1;
+            check_winner()
             turn = 1;
         }
         else {
             board[i].textContent = player2;
+            check_winner()
             turn = 0;
         }
     })
 }
+function check_winner() {
+    for (let i = 0; i < total_rows; i++) {
+        if ( (board[ways_to_win[i][0]].textContent != '') &&
+             (board[ways_to_win[i][0]].textContent == board[ways_to_win[i][1]].textContent) &&
+             (board[ways_to_win[i][1]].textContent == board[ways_to_win[i][2]].textContent) ) {
+                console.log(board[ways_to_win[i][0]].textContent + ' wins');
+             }
+    }
+};
